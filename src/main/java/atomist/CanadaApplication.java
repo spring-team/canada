e8ad3ee7e00e7337fb25cfbbdff911d7d0187ba4
Rod Johnson <johnsonroda@gmail.com>
2018-02-25 19:02:08 -0800

    Initial commit from Atomist

---
+++ src/main/java/atomist/CanadaApplication.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanadaApplication.class, args);
+	}
+}
